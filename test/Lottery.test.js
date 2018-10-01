6c1f81291ddcf66907114368bd6ce79e7d3d7535
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-01 21:40:32 +0530

    2 extra tests added finally

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -96,7 +96,7 @@ describe('Lottery Contract', ()=> {
     assert(difference > web3.utils.toWei('1.8', 'ether'));
   });
 
-  it('balance of lottery is zero after execution of pickWinner function', async ()=> {
+  it('lottery resets after execution of pickWinner function', async ()=> {
     await lottery.methods.enter().send({
       from: accounts[0], value: web3.utils.toWei('4', 'ether')
     });
@@ -104,5 +104,15 @@ describe('Lottery Contract', ()=> {
     await lottery.methods.pickWinner().send({from: accounts[0]});
 
     assert(players.length, undefined );
+  });
+
+  it('lottery has a balance of zero after one round', async ()=> {
+    await lottery.methods.enter().send({
+      from: accounts[0], value: web3.utils.toWei('2', 'ether')
+    });
+    const players = await lottery.methods.getPlayers().call({from: accounts[0]});
+    await lottery.methods.pickWinner().send({from: accounts[0]});
+    const Balance = await web3.eth.getBalance(players[0]);
+    assert(Balance, 0);
   });
 });
