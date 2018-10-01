323a0a90860727ad310b5f5334785417d7b5bdcf
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-01 17:03:31 +0530

    testing boilerplate progress

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -14,3 +14,9 @@ beforeEach(async ()=> {
     .deploy({data: bytecode})
     .send({from: accounts[0], gas = '1000000'});
 });
+
+describe('Lottery Contract', ()=> {
+  it('deploys a contract', ()=> {
+    assert.ok(lottery.options.address);
+  });
+});
