33e9ca54bb294a9b94a606b81edd61c35f61c515
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-01 18:01:56 +0530

    allows multiple accounts to enter

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -42,4 +42,21 @@ describe('Lottery Contract', ()=> {
     assert.equal(accounts[1], players[0]);
     assert.equal(1, players.length);
   });
+
+  it('allows multiple accounts to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    const players = await lottery.methods.getPlayers().call({from: accounts[0]});
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(accounts[1], players[1]);
+    assert.equal(2, players.length);
+  });
 });
