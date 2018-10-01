1c426a7c62084ee0bdddae1bad3bb3fa5eb93e84
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-01 16:57:02 +0530

    testing boilerplate

---
+++ test/Lottery.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const {interface, bytecode} = require('../compile');
+
+let lottery;
+let accounts;
+
+beforeEach(async ()=> {
+  accounts = web3.eth.getAccounts();
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode})
+    .send({from: accounts[0], gas = '1000000'});
+});
