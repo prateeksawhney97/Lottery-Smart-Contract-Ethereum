42b44833548e6613c0a8f5d4c82f31f301d59717
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-01 18:34:51 +0530

    only Manager can call pickWinner

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -70,4 +70,15 @@ describe('Lottery Contract', ()=> {
     assert(err);
   }
   });
+
+  it('only Manager can call pickWinner', async ()=> {
+    try {
+    await lottery.methods.pickWinner().send({
+      from: accounts[1]
+    });
+    assert(false);
+  } catch(err) {
+    assert(err);
+  }
+  });
 });
